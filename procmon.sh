50270a4e6c25743a45d8e62fcb1ec0fcf9b3e20f
Samuel Rencoret <119036636+sarengo@users.noreply.github.com>
2025-01-26 00:03:25 +0000

    Edit procmon.sh

--- procmon.sh
+++ procmon.sh
@@ -5,6 +5,6 @@ first_process=$(ps -eo user,command)
 while true; do
   second_process=$(ps -eo user,command)
   diff <(echo "$first_process") <(echo "$second_process") | grep [\<\>] | grep -vE "procmon|kworker|command"
-  first_process=$new_process
+  first_process=$second_process
 
 done
